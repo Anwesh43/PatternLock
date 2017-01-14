b7699502dd0c3de087c100ef939e72d65db1a1dc
Anwesh43 <anweshthecool0@gmail.com>
2017-01-14 13:47:30 +0530

    Adding constants file

---
+++ lockerpattern/src/main/java/com/anwesome/ui/lockerpattern/LPConstants.java
@@ -0,0 +1,13 @@
+package com.anwesome.ui.lockerpattern;
+
+/**
+ * Created by anweshmishra on 14/01/17.
+ */
+public class LPConstants {
+    public static final String SHARED_PREF_FILE  = "lock_indexes";
+    public static final String SAVED_KEY = "saved";
+    public static final String INDEXES_KEY="indexes";
+    public static final String SAVED_TEXT="DRAW YOUR PATTERN TO SAVE IT";
+    public static final String UNLOCK_TEXT="DRAW YOUR PATTERN TO UNLOCK";
+    public static final String WRONG_TEXT = "WRONG PATTERN TRY AGAIN";
+}
