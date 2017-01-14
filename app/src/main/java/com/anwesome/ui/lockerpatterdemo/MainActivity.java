62e56da98cd3d9374ec8b3423a66c21f7c6b7f69
Anwesh43 <anweshthecool0@gmail.com>
2017-01-14 13:55:46 +0530

    Adding some buttons

--- app/src/main/java/com/anwesome/ui/lockerpatterdemo/MainActivity.java
+++ app/src/main/java/com/anwesome/ui/lockerpatterdemo/MainActivity.java
@@ -1,16 +1,33 @@
 package com.anwesome.ui.lockerpatterdemo;
 
+import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.anwesome.ui.lockerpattern.LockerPattern;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button b1,b2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        b1 = (Button)findViewById(R.id.b1);
+        b2 = (Button)findViewById(R.id.b2);
+        b1.setOnClickListener(new Button.OnClickListener(){
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this,"Click1",Toast.LENGTH_SHORT).show();
+            }
+        });
+        b2.setOnClickListener(new Button.OnClickListener(){
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this,"Click2",Toast.LENGTH_SHORT).show();
+            }
+        });
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         LockerPattern lockerPattern = new LockerPattern(this);
         lockerPattern.lock();
     }
