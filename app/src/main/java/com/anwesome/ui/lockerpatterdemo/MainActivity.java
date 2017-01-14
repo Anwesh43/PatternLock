e770a226c445d6096ae2b66cc83c431ae0b31ae4
Anwesh43 <anweshthecool0@gmail.com>
2017-01-14 11:46:38 +0530

    the lockpattern demo part

--- app/src/main/java/com/anwesome/ui/lockerpatterdemo/MainActivity.java
+++ app/src/main/java/com/anwesome/ui/lockerpatterdemo/MainActivity.java
@@ -3,11 +3,15 @@ package com.anwesome.ui.lockerpatterdemo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.anwesome.ui.lockerpattern.LockerPattern;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        LockerPattern lockerPattern = new LockerPattern(this);
+        lockerPattern.lock();
     }
 }
